b04a15fbc56eff60c72616d8d0ebf48f640b38e8
Yuri Cesar <ycesar4273@gmail.com>
2026-04-30 19:35:25 -0300

    User entity and resource

---
+++ src/main/java/com/br/yuri/course/resources/UserResorce.java
@@ -0,0 +1,21 @@
+package com.br.yuri.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.yuri.course.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResorce {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+}
